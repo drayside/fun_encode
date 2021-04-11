51c7f46b99c2be4315bdff48f869244276f5e27d
Derek Rayside <drayside@uwaterloo.ca>
2021-04-11 07:16:01 -0400

    script to encode messages in DNA codons

---
+++ dna.php
@@ -0,0 +1,48 @@
+<?php
+
+# initialize encoding table
+$letter2codons = array(); # string -> list<string>
+$letters = range('A', 'Z');
+foreach ($letters as $letter) {
+    $letter2codons[$letter] = array();
+}
+$letter2codons["."] = array();
+
+$myfile = fopen("codon.txt", "r") or die("Unable to open file!");
+while(!feof($myfile)) {
+    $line = fgets($myfile);
+    # skip comment lines or empty lines
+    if (strlen($line) == 0 or strncmp($line, "#", 1) == 0) {
+        continue;
+    } else {
+        # we have data
+        $a = explode("\t", $line);
+        #var_dump($a);
+        $letter = $a[2];
+        $codon = $a[0];
+        # append this codon to the list for this letter
+        $letter2codons[$letter][] = $codon;
+    }
+}
+fclose($myfile);
+
+#var_dump($letter2codons);
+
+$string = "HELLO";
+echo $string . "\n";
+for ($i=0; $i<strlen($string); $i++) {
+    $letter = $string[$i];
+    $codons = $letter2codons[$letter];
+    $count = count($codons);
+    if ($count == 0) {
+        # letter has no codons
+        # use one of the STOP codons
+        $codons = $letter2codons['.'];
+        $count = count($codons);
+    }
+    $choice = rand(0, $count-1);
+    echo $codons[$choice] . " ";
+}
+echo "\n";
+
+?>
